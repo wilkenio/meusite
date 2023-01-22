0e44ff5d11f15b156e2ad2865b44e186a97bda20
Wilkenio Pereira <86073748+wilkenio@users.noreply.github.com>
2023-01-22 15:08:03 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+//funções do menu
 function menuabrir(iconHam){
     var menumobile = document.getElementById("menu-mobile")
     var x = document.getElementById("x")
@@ -16,14 +17,22 @@ function menufechar(x){
     menumobile.style.left="-100%"
 }
 
+//função para mudar a cor do menu de acordo com o scroll
+ let doc = document.documentElement
+ window.addEventListener('scroll', function() {
+     let value = parseInt(100 * doc.scrollTop / (doc.scrollHeight - doc.clientHeight))
+     // console.log(value)
+     if(value > 5){
+              document.getElementById("nav").style.background="#030912"
+     }else{
+         document.getElementById("nav").style.background="#02091233"
+     }
+ })
 
-let doc = document.documentElement
-window.addEventListener('scroll', function() {
-    let value = parseInt(100 * doc.scrollTop / (doc.scrollHeight - doc.clientHeight))
-    // console.log(value)
-    if(value > 10){
-         document.getElementById("nav").style.background="#030912"
-    }else{
-        document.getElementById("nav").style.background="#02091233"
-    }
-})
+//fechar menu apos clique algum item
+$(".itens-menu").click(function(event){
+   if(window.screen.availWidth < 1050){
+    var ham = document.getElementById("x")
+    menufechar(ham)
+   }
+});
